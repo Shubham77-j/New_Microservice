b1cdfbab980a2ea29328d946db19108a4c278474
Shubham <pradhanshubham52@gmail.com>
2025-05-27 16:21:53 +0530

    area

--- src/main/java/com/propertyservice/entity/Area.java
+++ src/main/java/com/propertyservice/entity/Area.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+// this is area
 @Entity
 @Table(name="area")
 public class Area {
